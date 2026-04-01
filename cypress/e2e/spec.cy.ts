a4d44aa00d72b5a1d040c5e5dd1eeefea5b0eefa
bigcloud07 <dnsxo0712@gmail.com>
2026-04-01 16:40:48 +0900

    feat: 영화 목록 조회 기능 추가
    Co-authored-by: bel1c10ud <bel1c10ud@users.noreply.github.com>

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,47 +4,39 @@ describe("template spec", () => {
   });
 
   context("Movie List 탐색", () => {
-    cy.get(".item").should('have.length', 20);
-    cy.get('.show-more-button');
-    cy.get('.show-more-button').click();
-    cy.get(".item").should('have.length', 40);
+    cy.get(".item").should("have.length", 20);
+    cy.get(".show-more-button");
+    cy.get(".show-more-button").click();
+    cy.get(".item").should("have.length", 40);
   });
 
   context("검색 - 결과 있음", () => {
-    cy.get('.search-input').type("Inception");
+    cy.get(".search-input").type("Inception");
     cy.get(".search-submit").click();
-    cy.get(".item")
-      .its('length')
-      .should('be.gte', 1)
+    cy.get(".item").its("length").should("be.gte", 1);
   });
 
   context("검색 - 결과 있는 경우의 재검색", () => {
-    cy.get('.search-input').type("Inception");
+    cy.get(".search-input").type("Inception");
     cy.get(".search-submit").click();
-    cy.get(".item")
-      .its('length')
-      .should('be.gte', 1)
-    cy.get('.search-input').type("Inception");
+    cy.get(".item").its("length").should("be.gte", 1);
+    cy.get(".search-input").type("Inception");
     cy.get(".search-submit").click();
-    cy.get(".item")
-      .its('length')
-      .should('be.gte', 1)
+    cy.get(".item").its("length").should("be.gte", 1);
   });
 
   context("검색 - 결과 없음", () => {
-    cy.get('.search-input').type("$$%@@");
+    cy.get(".search-input").type("$$%@@");
     cy.get(".search-submit").click();
     cy.contains("검색 결과가 없습니다");
   });
-  
+
   context("검색 - 결과 없는 경우의 재검색", () => {
-    cy.get('.search-input').type("$$%@@");
+    cy.get(".search-input").type("$$%@@");
     cy.get(".search-submit").click();
     cy.contains("검색 결과가 없습니다");
-    cy.get('.search-input').type("Inception");
+    cy.get(".search-input").type("Inception");
     cy.get(".search-submit").click();
-    cy.get(".item")
-      .its('length')
-      .should('be.gte', 1)
+    cy.get(".item").its("length").should("be.gte", 1);
   });
 });
